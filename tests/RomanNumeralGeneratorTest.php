ab494d8be8c1e8dc9b920639bb38fc956d756c83
development <development@jonathansblog.co.uk>
2014-08-21 21:27:56 +0100

    initial php file and rest file

---
+++ tests/RomanNumeralGeneratorTest.php
@@ -0,0 +1,18 @@
+<?php
+class RomanNumeralGeneratorTest extends PHPUnit_Framework_TestCase
+{
+    public function testgenerate()
+    {
+        // Arrange
+        $a = new RomanNumeral();
+
+    }
+    
+    public function testparse()
+    {
+        // Arrange
+        $a = new RomanNumeral();
+    
+    }
+
+}
