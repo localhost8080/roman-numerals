32bc1bc2d31d0f1f226d1862a16f0e1efad65acf
development <development@jonathansblog.co.uk>
2014-08-23 14:14:14 +0100

    test

--- tests/classes/RomanNumeralGeneratorTest.php
+++ tests/classes/RomanNumeralGeneratorTest.php
@@ -74,7 +74,6 @@ class RomanNumeralTest extends PHPUnit_Framework_TestCase
         // test
         $this->assertEquals('CMXCIX',$result);        
         
-
         $result = $a->parse(2014);
         // test
         $this->assertEquals('MMXIV',$result);
