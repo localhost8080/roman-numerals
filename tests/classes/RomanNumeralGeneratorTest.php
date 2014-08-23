a1279cccea7b7de7d2574488c3cdccbdf444c37e
development <development@jonathansblog.co.uk>
2014-08-23 16:43:08 +0100

    initial exception testing

--- tests/classes/RomanNumeralGeneratorTest.php
+++ tests/classes/RomanNumeralGeneratorTest.php
@@ -1,145 +1,201 @@
 <?php
 // normally we would have an autoloader
-include_once('../../classes/RomanNumeralGenerator.php');
-
+include_once ('../../classes/RomanNumeralGenerator.php');
 
 class RomanNumeralTest extends PHPUnit_Framework_TestCase
 {
+
     public function testgenerate()
     {
         // Arrange
         $a = new RomanNumeral();
-        
         // some tests, by far these are not great
-        
         $result = $a->generate(1);
         // test
-        $this->assertEquals('I',$result);
-
+        $this->assertEquals('I', $result);
         $result = $a->generate(2);
         // test
-        $this->assertEquals('II',$result);
-        
+        $this->assertEquals('II', $result);
         $result = $a->generate(3);
         // test
-        $this->assertEquals('III',$result);
-        
+        $this->assertEquals('III', $result);
         $result = $a->generate(5);
         // test
-        $this->assertEquals('V',$result);
-        
+        $this->assertEquals('V', $result);
         $result = $a->generate(10);
         // test
-        $this->assertEquals('X',$result);
-        
+        $this->assertEquals('X', $result);
         $result = $a->generate(50);
         // test
-        $this->assertEquals('L',$result);
-        
+        $this->assertEquals('L', $result);
         $result = $a->generate(100);
         // test
-        $this->assertEquals('C',$result);
-        
+        $this->assertEquals('C', $result);
         $result = $a->generate(500);
         // test
-        $this->assertEquals('D',$result);
-        
+        $this->assertEquals('D', $result);
         $result = $a->generate(1000);
         // test
-        $this->assertEquals('M',$result);
-        
+        $this->assertEquals('M', $result);
         // trickier ones
-        
         $result = $a->generate(4);
         // test
-        $this->assertEquals('IV',$result);
-        
+        $this->assertEquals('IV', $result);
         $result = $a->generate(9);
         // test
-        $this->assertEquals('IX',$result);
-        
+        $this->assertEquals('IX', $result);
         $result = $a->generate(90);
         // test
-        $this->assertEquals('XC',$result);
-
+        $this->assertEquals('XC', $result);
         $result = $a->generate(400);
         // test
-        $this->assertEquals('CD',$result);
-        
+        $this->assertEquals('CD', $result);
         $result = $a->generate(900);
         // test
-        $this->assertEquals('CM',$result);
-        
+        $this->assertEquals('CM', $result);
         $result = $a->generate(999);
         // test
-        $this->assertEquals('CMXCIX',$result);        
-        
+        $this->assertEquals('CMXCIX', $result);
         $result = $a->generate(2014);
         // test
-        $this->assertEquals('MMXIV',$result);
-        
+        $this->assertEquals('MMXIV', $result);
         // what happens when we pass an incorrect data type
         $result = $a->generate('string');
         // test
         $this->assertFalse($result);
-        
     }
-    
+
     public function testparse()
     {
         // Arrange
         $a = new RomanNumeral();
-        
-        
         // some tests, by far these are not great
-        
-        $result = $a->parse('I'); 
+        $result = $a->parse('I');
         // test
-        $this->assertEquals(1,$result);
-        
+        $this->assertEquals(1, $result);
         $result = $a->parse('V');
         // test
-        $this->assertEquals(5,$result);
-        
+        $this->assertEquals(5, $result);
         $result = $a->parse('IV');
         // test
-        $this->assertEquals(4,$result);
-        
+        $this->assertEquals(4, $result);
         $result = $a->parse('VI');
         // test
-        $this->assertEquals(6,$result);
-
+        $this->assertEquals(6, $result);
         $result = $a->parse('X');
         // test
-        $this->assertEquals(10,$result);
-        
+        $this->assertEquals(10, $result);
         $result = $a->parse('IX');
         // test
-        $this->assertEquals(9,$result);
-        
+        $this->assertEquals(9, $result);
         $result = $a->parse('XI');
         // test
-        $this->assertEquals(11,$result);
-        
+        $this->assertEquals(11, $result);
         $result = $a->parse('XX');
         // test
-        $this->assertEquals(20,$result);
-        
+        $this->assertEquals(20, $result);
         $result = $a->parse('XIX');
         // test
-        $this->assertEquals(19,$result);
-
+        $this->assertEquals(19, $result);
         $result = $a->parse('MMXIV');
         // test
-        $this->assertEquals(2014,$result);
-        
+        $this->assertEquals(2014, $result);
         // what if we pass an incorect data type
-        
         $result = $a->parse(1000);
         // test
         $this->assertFalse($result);
+    }
+
+    public function testvalidate_roman()
+    {
+        // Arrange
+        $a = new RomanNumeral();
+        // some tests, by far these are not great
+        $result = $a->validate_roman('I');
+        // test
+        $this->assertTrue($result);
+        
+        $result = $a->validate_roman('V');
+        // test
+        $this->assertTrue($result);
+        
+        $result = $a->validate_roman('X');
+        // test
+        $this->assertTrue($result);
+        
+        $result = $a->validate_roman('C');
+        // test
+        $this->assertTrue($result);
+        
+        // not a valid roman number
+        try {
+            // check if valid
+            $result = $a->validate_roman(4);
+        } catch (Exception $expected) {
+            return $expected->getMessage();
+        }
+        
+        // not a valid roman number
+        try {
+            // check if valid
+            $result = $a->validate_roman('F');
+        } catch (Exception $expected) {
+            return $expected->getMessage();
+        } 
         
         
     }
 
+    public function testvalidate_arabic()
+    {
+        // Arrange
+        $a = new RomanNumeral();
+        // some tests, by far these are not great
+        $result = $a->validate_arabic(1);
+        // test
+        $this->assertTrue($result);
+        
+        $result = $a->validate_arabic(4);
+        // test
+        $this->assertTrue($result);
+        
+        $result = $a->validate_arabic(15);
+        // test
+        $this->assertTrue($result);
+        
+        
+        // not valid ones
+        
+        try {
+            // check if valid
+            $result = $a->validate_arabic(4000);
+        } catch (Exception $expected) {
+            return $expected->getMessage();
+        }
+        
+        // not a valid roman number
+        try {
+            // check if valid
+            $result = $a->validate_arabic(400000000);
+        } catch (Exception $expected) {
+            return $expected->getMessage();
+        }
+        
+        try {
+            // check if valid
+            $result = $a->validate_arabic('random string of things');
+        } catch (Exception $expected) {
+            return $expected->getMessage();
+        }
+        
+        // not a valid roman number
+        try {
+            // check if valid
+            $result = $a->validate_arabic('M');
+        } catch (Exception $expected) {
+            return $expected->getMessage();
+        }
+        
+        
+    }
 }
