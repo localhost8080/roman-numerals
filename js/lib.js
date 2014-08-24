3698d610353d661d2c032564c1541dd4cb03cc15
development <development@jonathansblog.co.uk>
2014-08-24 11:35:44 +0100

    validating with jshint in brackets

--- js/lib.js
+++ js/lib.js
@@ -1,8 +1,14 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+// above are some comments for jshint - so that it understands jquery is defined in another file
+
 // these could be simplified and generalised;
 // eg call_api /api?method={$_REQUEST['method']}
 // also, there should be error checking and sanitisation of user input at this stage
 
 function call_parse_api(object) {
+    "use strict";
     // call the parse function through our mini api
     $.getJSON('/api/?method=parse', object.serialize(), function (data) {
         $('#string_results').html(data);
@@ -10,6 +16,7 @@ function call_parse_api(object) {
 }
 
 function call_generate_api(object) {
+    "use strict";
     // call the parse function through our mini api
     $.getJSON('/api/?method=generate', object.serialize(), function (data) {
         $('#number_results').html(data);
@@ -17,6 +24,7 @@ function call_generate_api(object) {
 }
 
 $(function () {
+    "use strict";
     // bind keypresses on the form input to trigger the validator
     $('#number').on("input", function () {
         call_generate_api($('#number'));
